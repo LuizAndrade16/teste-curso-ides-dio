c97dc35a69b45d02bb1f5fbf571f36c55db4c2ee
Luiz Paulo <luizandrade1601@gmail.com>
2022-04-02 13:58:40 -0300

    Second test

---
+++ src/BR/COM/DIO/PrimeiroProjeto.java
@@ -0,0 +1,15 @@
+package BR.COM.DIO;
+
+import BR.COM.DIO.MODEL.Gato;
+
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+        Gato gato = new Gato ();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World!" + (a+b));*/
+    }
+}
